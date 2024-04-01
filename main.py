a4ffda328ceb1892f552f78289c655666ae60fb1
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-04-01 14:43:47 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import rsa
 
 
+# Define the number of messages that we want to encrypt (in our case,
+# we want to decrypt two cipher texts, so we initialize it = 2)
+NUM_MESSAGES = 2
+
+
 # Function that, given the private key file path (string) and the
 # cipher text file path (string), will decrypt the message contents
 # in the cipher text file and return them in binary format.
@@ -23,18 +28,17 @@ def decrypt(priv_key_file: str, cipher_file: str) -> bytes:
 
 # Run the code as the main entry point to the program
 if __name__ == "__main__":
-    print("MESSAGE #1\n")
-    # Call the function decrypt for cipher text #1
-    msg1 = decrypt(priv_key_file="prv.key", cipher_file="cipher1.bin").decode("utf-8")
-    # Print it and pipe the contents to output1.txt
-    print(msg1)
-    with open("output1.txt", "w", encoding="utf-8") as text_file:
-        text_file.write(msg1)
+    # Decrpyt each of the cipher text files specfied by
+    # the total number of messages to decrypt
+    for i in range(NUM_MESSAGES):
+        print(f"MESSAGE #{i+1}\n")
+        # Call the function decrypt for cipher text #1
+        msg = decrypt(priv_key_file="prv.key", cipher_file=f"cipher{i+1}.bin").decode(
+            "utf-8"
+        )
+        # Print it and pipe the contents to output1.txt
+        print(msg, "\n")
+        with open(f"output{i+1}.txt", "w", encoding="utf-8") as text_file:
+            text_file.write(msg)
 
-    print("\nMESSAGE #2\n")
-    # Call the function decrypt for cipher text #2
-    msg2 = decrypt(priv_key_file="prv.key", cipher_file="cipher2.bin").decode("utf-8")
-    # Print it and pipe the contents to output2.txt
-    print(msg2)
-    with open("output2.txt", "w", encoding="utf-8") as text_file:
-        text_file.write(msg2)
+    print("Finished decrypting all messages!")
